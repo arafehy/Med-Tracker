a71e856622faf36474eb0eb3ee2389b2ffeedce9
Yazan Arafeh <yazanarafeh24@gmail.com>
2020-09-01 13:26:38 -0700

    Project setup

---
+++ Med Tracker/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Med Tracker
+//
+//  Created by Yazan Arafeh on 9/1/20.
+//  Copyright © 2020 Yazan Arafeh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
