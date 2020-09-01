a0f13502e90baa1cbab893b8f67965ae37620128
Yazan Arafeh <yazanarafeh24@gmail.com>
2020-09-01 16:09:13 -0700

    Created medication detail view

---
+++ Med Tracker/Views/MedDetail.swift
@@ -0,0 +1,49 @@
+//
+//  MedDetail.swift
+//  Med Tracker
+//
+//  Created by Yazan Arafeh on 9/1/20.
+//  Copyright © 2020 Yazan Arafeh. All rights reserved.
+//
+
+import SwiftUI
+
+struct MedDetail: View {
+    var medication: MedicationItem
+    
+    var body: some View {
+        VStack {
+            medImage
+            medCount
+            medInstructions
+            Spacer()
+        }
+    }
+    
+    var medImage: some View {
+        Image(medication.image)
+            .resizable()
+            .scaledToFit()
+    }
+    
+    var medCount: some View {
+        Text("Remaining: \(medication.count)")
+            .font(.largeTitle)
+            .bold()
+    }
+    
+    var medInstructions: some View {
+        VStack(alignment: .leading) {
+            Text("Instructions")
+                .font(.title)
+            Text(medication.instructions)
+        }
+        .padding()
+    }
+}
+
+struct MedDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        MedDetail(medication: MedicationItem.example)
+    }
+}
